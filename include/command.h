deb29f8b9207d132b6e96c26bd690fbb909fcb28
wonpark1 <2023111927@dgu.ac.kr>
2025-05-20 15:16:46 +0900

    command_h_c

--- include/command.h
+++ include/command.h
@@ -8,5 +8,19 @@ void command_mkdir(int argc, char** argv);
 void command_cat(DTree* tree, const char* filename);
 void command_alias(AliasTable* table, int argc, char** argv);
 void command_rm(int argc, char** argv);
+int command_cd  (DTree *tree, UList *users, int argc, char **argv);
+int command_pwd (DTree *tree, Stack *stack, UList *users);
+int command_cp  (DTree *tree, int argc, char **argv);
 
+int  cd         (DTree *tree, char *command);
+int  MovePath   (DTree *tree, char *path);
+int  Movecurrent(DTree *tree, char *segment);
+
+int  pwd        (DTree *tree, Stack *stack, char *opt);
+int  PrintPath  (DTree *tree, Stack *stack);
+
+int  cp         (DTree *tree, char *src, char *dst);
+
+/* UList 의 dir 멤버를 현재 경로로 갱신 */
+void UpdateUserDir(UList *users, DTree *tree);
 #endif

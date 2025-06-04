1c58f1f4cd78804ad967ba2ed1e8a475d350029d
ybkim453 <yb1553@dgu.ac.kr>
2025-06-04 16:08:27 +0900

    Final code

--- include/ls.h
+++
@@ -1,8 +0,0 @@
-#ifndef LS_H
-#define LS_H
-
-#include "osproject.h"
-
-void command_ls(int argc, char** argv);
-
-#endif
--- include/parser.h
+++
@@ -1,9 +0,0 @@
-// include/parser.h
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "osproject.h"
-
-void ParseAndExecute(DTree* tree, Stack* stack, UList* users, AliasTable* aliasTable, char* input);
-
-#endif
